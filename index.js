2f4cf6c562ad9dca2b7785c48d609d276f35e066
Mior Sufian <mior.sufian@googlemail.com>
2026-09-24 04:12:51 +0800

    added author

--- index.js
+++ index.js
@@ -67,11 +67,11 @@ app.get('/posts', async (req, res) => {
 });
 
 app.post('/posts', async (req, res) => {
-    const { title, content, user_id } = req.body;
+    const { title, content, author, user_id } = req.body;
     try {
         const result = await pool.query(
-            'INSERT INTO posts (title, content, user_id) VALUES ($1, $2, $3) RETURNING *',
-            [title, content, user_id]
+            'INSERT INTO posts (title, content, author, user_id) VALUES ($1, $2, $3, $4) RETURNING *',
+            [title, content, author, user_id]
         );
         res.status(201).json(result.rows[0]);
     } catch (err) {
