39738b6ab385e3fb15ca352787fae14aa6951e6d
Mior Sufian <mior.sufian@googlemail.com>
2026-09-24 10:57:07 +0800

    added delete and patch http verbs

--- index.js
+++ index.js
@@ -80,8 +80,44 @@ app.post('/posts', async (req, res) => {
     }
 });
 
+//////////////////////////
+//delete
+app.delete('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    try {
+        const result = await pool.query('DELETE FROM posts WHERE id = $1 RETURNING *', [id]);
+        if (result.rows.length === 0) {
+            return res.status(404).json({ error: 'Posts not found' });
+        }
+        res.json({ message: 'Posts deleted', post: result.rows[0] });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+//end of delete
+///////////////////////////
 
 
+////////////////////////////
+//start of patch
+app.patch('/posts/:id', async (req, res) => {
+    const { id } = req.params;
+    const { title, content, author, user_id } = req.body;
+    try {
+        const result = await pool.query(
+            'UPDATE posts SET (title, content, author, user_id) VALUES ($1, $2, $3, $4) WHERE id = $5 RETURNING *',
+            [title, content, author, user_id, id]
+        );
+        res.status(201).json(result.rows[0]);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ error: 'Something went wrong' });
+    }
+});
+
+
+////////////////////////////
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Route not found' });
